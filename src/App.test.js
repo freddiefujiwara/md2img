93ac2016faf4e05efecb24db72d5025e4fad050d
Fumikazu Fujiwara | Freddie <github@ze.gs>
2026-02-05 12:26:49 +0900

    test: use exposed ref in capture test

--- src/App.test.js
+++ src/App.test.js
@@ -205,7 +205,7 @@ describe("App", () => {
     const { app, vm } = mountApp();
     await waitForPaginate();
 
-    vm.$.setupState.pageCaptureRef.value = null;
+    vm.$.exposed.pageCaptureRef.value = null;
     await vm.exportAllPng();
 
     expect(html2canvas).not.toHaveBeenCalled();
